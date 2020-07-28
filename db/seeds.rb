40169d62ed7eaad06df957ec51632327f9a6fb3a
Bruno Tostes <btfjulio@hotmail.com>
2020-07-28 16:17:14 -0300

    add mogrations to app

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+Supplement.create([
+  {
+    name: '100% WHEY GOLD STANDARD (907g + 181g) - Chocolate - Optimum Nutrition',
+    image_url: 'https://produto.saudifitness.com.br//460x460/10112169899.jpg/flags?aplicarFlags=true&amp;unidade=4&amp;v=11'
+  },
+  {
+    name: 'WHEY BLEND AGE (2Kg) - Chocolate - AGE',
+    image_url: 'https://produto.saudifitness.com.br//460x460/10112174303.jpg/flags?aplicarFlags=true&amp;unidade=4&amp;v=11'
+  },
+  {
+    name: 'WHEY 100% PURE (1,8Kg - Refil) - Chocolate - IntegralMedica',
+    image_url: 'https://produto.saudifitness.com.br//460x460/1011189462.jpg/flags?aplicarFlags=true&amp;unidade=4&amp;v=11'
+  },
+  {
+    name: 'BARRA DE PROTEÍNA VO2 DISPLAY C/ 24 UNIDADES (720g) - Cookies - IntegralMedica',
+    image_url: 'https://produto.saudifitness.com.br//460x460/1011210742.jpg/flags?aplicarFlags=true&amp;unidade=4&amp;v=11'
+  },
+  {
+    name: 'SUPER WHEY (900g - Refil) - Chocolate - Max Titanium',
+    image_url: 'https://produto.saudifitness.com.br//460x460/10112174431.jpg/flags?aplicarFlags=true&amp;unidade=4&amp;v=11'
+  }
+])
